3a220416a4122527a573651efa94111be75f3c3e
hafiz4m1 <133829025+hafiz4m1@users.noreply.github.com>
2025-01-14 20:27:14 +0800

    second kommit

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Baramg extends Model
+class Barang extends Model
 {
     use HasFactory;
 
     protected $table = 'barang';
-    protected $guarded = ['id'];
+    protected $guarded =  ['id'];
     public $timestamps = false;
 }
